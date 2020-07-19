b1a82333d778dc9ddb77f9775924ae135ecf2f4e
ShowGoTagami <whitenotesfor@gmail.com>
2020-07-19 12:44:26 +0900

    add constants and images

---
+++ frontend/src/constants.js
@@ -0,0 +1,9 @@
+export const REQUEST_STATE = {
+  INITIAL: 'INITIAL',
+  LOADING: 'LOADING',
+  OK: 'OK',
+}
+
+export const HTTP_STATUS_CODE = {
+  NOT_ACCEPTABLE: 406,
+}
---
+++ frontend/src/style_constants.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  MAIN: '#02A457',
+  BORDER: '#e2e2e2',
+  SUB_TEXT: '#545454',
+  SUB_BUTTON: '#eeeeee',
+}
+
+export const FONT_SIZE = {
+  STAND_BODY: '24px',
+  BODY1: '16px',
+  BODY2: '14px',
+}
